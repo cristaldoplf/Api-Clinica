150735c85880b43bc0d0fe18792ae9f16c8a6fa5
cristaldoplf <cristaldoplf@gmail.com>
2022-04-03 08:51:09 -0300

    presentacion en pantalla de la lista de odontologos consultadas mediante fetch

--- Backend/clinica/src/main/java/com/example/clinica/repository/IDaoPaciente.java
+++
@@ -1,8 +0,0 @@
-package com.example.clinica.repository;
-
-public interface IDaoPaciente<T> {
-    public T buscarPorEmail(String email);
-
-    public T editar(T t);
-
-}
---
+++ Frontend/proyecto-clinica/src/components/Design.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ListaOdontologos from "./ListaOdontologos";
+
+const Design = () => {
+
+  return (
+    <>
+      <h1>Odontologos</h1>
+      <ListaOdontologos/>
+    </>
+  )
+
+}
+
+
+export default Design;
